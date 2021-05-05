ccc7d7c9d4f9b40352ae0606641cc79dfa584bf8
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-05 11:09:33 -0300

    FINAL

--- EXERCISE 01 - TIMESPAN OPERATIONS M.07/Program.cs
+++ EXERCISE 01 - TIMESPAN OPERATIONS M.07/Program.cs
@@ -17,6 +17,21 @@ namespace EXERCISE_01___TIMESPAN_OPERATIONS_M._07 {
             Console.WriteLine("TotalMinutes: " + t.TotalMinutes);
             Console.WriteLine("TotalSeconds: " + t.TotalSeconds);
             Console.WriteLine("TotalMilliseconds: " + t.TotalMilliseconds);
+
+            Console.WriteLine();    
+
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+            TimeSpan t2 = new TimeSpan(0, 10, 5);
+            TimeSpan sum = t1.Add(t2);
+            TimeSpan dif = t1.Subtract(t2);
+            TimeSpan mult = t2.Multiply(2.0);
+            TimeSpan div = t2.Divide(2.0);
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
         }
     }
     /////////////////////////////////////////////////////////////////////////////////////////////
